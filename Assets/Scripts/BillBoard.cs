f569dcc9baee2276c573ff64cbf9930b2b18c53b
Keegan Miller <keegancole2@gmail.com>
2024-02-05 18:23:35 -0600

    commit before merge

--- Assets/Scripts/BillBoard.cs
+++ Assets/Scripts/BillBoard.cs
@@ -6,17 +6,54 @@ public class BillBoard : MonoBehaviour
 {
     // Start is called before the first frame update
     Vector3 board;
+
+    Camera mainCamera;
+
     void Start()
     {
-        
+        // Initial attempt to find the main camera
+        TryFindMainCamera();
+    }
+
+    void TryFindMainCamera()
+    {
+        mainCamera = FindMainCamera();
+        if (mainCamera == null)
+        {
+            Debug.LogError("Main camera not found. Make sure there is a camera tagged as 'MainCamera' in your scene.");
+        }
+    }
+
+    Camera FindMainCamera()
+    {
+        Camera[] cameras = Camera.allCameras;
+        foreach (Camera cam in cameras)
+        {
+            if (cam.CompareTag("MainCamera"))
+            {
+                return cam;
+            }
+        }
+        return null;
     }
 
     // Update is called once per frame
     void Update()
     {
-        board = Camera.main.transform.forward;
-        board.y = 0;
+        // Check for the main camera each frame
+        if (mainCamera == null)
+        {
+            TryFindMainCamera();
+        }
 
-        transform.rotation = Quaternion.LookRotation(board);
+        // Proceed with the billboard behavior if the main camera is found
+        if (mainCamera != null)
+        {
+            board = mainCamera.transform.forward;
+            board.y = 0;
+            // transform.rotation = Quaternion.LookRotation(board);
+            board.y = 0;
+            
+        }
     }
 }
